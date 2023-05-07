2f1c42be7ee6aa3600256fd648349d03bc1051c7
Jason Tavera <taverajason7@gmail.com>
2023-05-06 19:24:49 -0500

    added sql starter code

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO role (title, salary, department_id)
+VALUES (),
+       (),
+       (),
+       ();
+       
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES (),
+       (),
+       (),
+       ();
+       
---
+++ index.mjs
@@ -0,0 +1,2 @@
+import mysql from "mysql2"
+import inquirer from "inquirer"
